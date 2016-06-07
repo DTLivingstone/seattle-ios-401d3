5ffb1a0bcd67b690d3a284e53769ac1c11d0aceb
David Livingstone <david@david.tl>
2016-06-07 13:41:05 -0700

    finished problem 3

--- week-01/class-2/Homework2.playground/Contents.swift
+++ week-01/class-2/Homework2.playground/Contents.swift
@@ -1,8 +1,8 @@
 //Write a function that takes in an array of strings, checks if array contains “Earth”, if not, adds it otherwise returns
 
-var bigPlanets = ["Jupiter", "Saturn"]
+var waterPlanets = ["Earth", "Mars"]
 
-var awesomePlanets = ["Mars", "Neptune", "Earth"]
+var awesomePlanets = ["Venus", "Neptune", "Jupiter"]
 
 func earthAdder(inout planetArr: [String]) -> [String]? {
     var hasEarth = false
@@ -20,16 +20,22 @@ func earthAdder(inout planetArr: [String]) -> [String]? {
     }
 }
 
-earthAdder(&bigPlanets)
+earthAdder(&waterPlanets)
 earthAdder(&awesomePlanets)
 
 //Use reduce to create single string from the above array. [“Venus”, “Earth”] would produce “Venus, Earth”
 
 
+
 //Declare and implement a dictionary that can contain as values Strings, Ints, etc
 
 
+var cityCoolness = [String: Int]()
+
+cityCoolness = ["Seattle": 3, "Portland": 1, "Athens": 8, "Branson": 999]
+
 //Declare a dictionary of string values. Declare a function that takes in a string and searches dictionary for it. Use filter
+
 
 
 //Write examples for union, intersect, subtract, exclusive Or set operations
