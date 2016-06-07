8294ad149b3c4ebb4ab10f736f683839ecae356c
David Livingstone <david@david.tl>
2016-06-07 14:46:20 -0700

    finished question 5, more work on other problems

--- week-01/class-2/Homework2.playground/Contents.swift
+++ week-01/class-2/Homework2.playground/Contents.swift
@@ -29,13 +29,33 @@ earthAdder(&awesomePlanets)
 
 //Declare and implement a dictionary that can contain as values Strings, Ints, etc
 
-
 var cityCoolness = [String: Int]()
 
 cityCoolness = ["Seattle": 3, "Portland": 1, "Athens": 8, "Branson": 999]
 
 //Declare a dictionary of string values. Declare a function that takes in a string and searches dictionary for it. Use filter
 
+let zootopiaCharacters = [1: "Judy Hopps", 2: "Nick Wilde", 3: "Emmet Otterton"]
 
+//func dictionarySearch(searchTerm: String) -> (Key: Int, Value: String)? {
+//    let searchResult = zootopiaCharacters.filter({$0 == searchTerm})
+//    return searchResult
+//}
+
+//dictionarySearch("Nick Wilde")
 
 //Write examples for union, intersect, subtract, exclusive Or set operations
+
+let gum: Set = ["wintergreen", "mint", "bubblegum"]
+let iceCream: Set = ["mint", "coffee", "strawberry"]
+
+let allFlavors = gum.union(iceCream)
+let sharedFlavors = gum.intersect(iceCream)
+let subtractFlavors = gum.subtract(iceCream)
+let exclusiveFlavors = gum.exclusiveOr(iceCream)
+
+//Write a function that counts repeating words and prints out the count of each
+
+
+//Add two numbers without using “+”
+
